74049392dd29a4773a46846545fa9eea5fcd7b3a
yuhgi <yuhgi777@163.com>
2018-02-22 16:39:59 +0800

    use .gitignore

---
+++ java-demo/Maven/my-java-app/src/main/java/com/mycompany/app/JavaApp.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class JavaApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "My first Java Project!" );
+    }
+}
