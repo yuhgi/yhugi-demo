39250a0aa2c902b076c50b57b5d6e66aafa6e5e2
yuhgi <yuhgi777@163.com>
2018-05-21 09:51:05 +0800

    react ssr - next

--- fe-demo/react-demo/next/hello-next/components/Header.js
+++ fe-demo/react-demo/next/hello-next/components/Header.js
@@ -7,10 +7,10 @@ const linkStyle={
 const Header = () => (
     <div>
         <Link href="/">
-          <a style={linkStyle}>Home</a>
+            <a style={linkStyle}>Home</a>
         </Link>
         <Link href="/about">
-          <a style={linkStyle}>About</a>
+            <a style={linkStyle}>About</a>
         </Link>
     </div>
 );
---
+++ fe-demo/react-demo/redux-demo/redux-saga/main.js
@@ -0,0 +1,31 @@
+import "babel-polyfill"
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga';
+
+import {helloSaga} from './sagas';
+
+import Counter from './Counter'
+import reducer from './reducers'
+
+const store = createStore(
+  reducer,
+  applyMiddleware(createSagaMiddleware(helloSaga))
+)
+
+const action = type => store.dispatch({type})
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => action('INCREMENT')}
+      onDecrement={() => action('DECREMENT')} />,
+    document.getElementById('root')
+  )
+}
+
+render()
+store.subscribe(render)
