4c8a48d9a5f2fbf4a9d4ea8d3636f5dc0520704c
yuhgi <yuhgi777@163.com>
2018-10-08 17:33:11 +0800

    echarts示例

---
+++ fe-demo/node-demo/node-nginx/forward.js
@@ -0,0 +1,8 @@
+const http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('remoteAddress: ' + req.connection.remoteAddress + '\n');
+    res.write('x-forwarded-for: ' + req.headers['x-forwarded-for'] + '\n');
+    res.write('x-real-ip: ' + req.headers['x-real-ip'] + '\n');
+    res.end();
+}).listen(9009, '0.0.0.0');
