bcda8ce547ce467f459b8053908a44a7b3dea181
yuhgi <yuhgi777@163.com>
2019-01-30 16:37:50 +0800

    maven基础教程

---
+++ java-demo/Maven-demo/snapshot/data-service/src/main/java/com/yuhgi/maven/DataService.java
@@ -0,0 +1,13 @@
+package com.yuhgi.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class DataService 
+{
+    public static void sayHi( )
+    {
+        System.out.println( "Hello  new World!" );
+    }
+}
